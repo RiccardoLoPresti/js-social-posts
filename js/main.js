66a2fb78689463a37db4aadc3dc4dc3fda735882
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-27 15:49:32 +0200

    [add] stamp HTML + stamp data good form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/*Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Utilizzando la base dati fornita e prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+Formattare le date in formato italiano (gg/mm/aaaa)*/
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +59,88 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+//prendo il container tramite id
+const container = document.getElementById('container');
+
+//richiamo la funzione init()
+init();
+
+//funzione init() svuota il container e stampa tramite stampPost()
+function init() {
+    //svuoto container
+    container.innerHTML ='';
+    //ciclo tutto l'array dei post
+    posts.forEach((post) => {
+        //richiamo la funzione che mi stampa i post in modo dinamico passando come vallore post
+        stampPost(post);
+    });
+}
+
+//funzione stampPost() mi stampa i post nell'HTML
+function stampPost(post) {
+    //salvo in una variabile tutti i dati che mi servono
+    const {id,content,media,author,likes,created} = posts;
+
+    //salvo la data rovesciata in un array
+    const arrayDate = dataReverse(post.created);
+
+    //la trasformo in stringa
+    const dateStr = dataString(arrayDate);
+
+    //sostituisco le virgole con il trattino
+    const date = dateStr.replace(/,/g,"-")
+
+    //salvo in element HTML da stampare in pagina
+    let element = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="NON C'è FOTO">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${date}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="${post.media}">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>
+        </div>
+    `;
+
+    container.innerHTML += element;
+    
+}
+
+function dataReverse(string) {
+    const arrayData = string.split('-');
+    return itaData = arrayData.reverse();
+}
+
+function dataString(string) {
+    return date = string.toString();
+}
+
+/*function getMonthDiff(todayDate,imgDate) {
+    let months;
+    months = (todayDate.getFullYear() - imgDate.getFullYear()) * 12;
+    months -= todayDate.getMonth();
+    months += imgDate.getMonth();
+    return months <= 0 ? 0 : months
+}*/
